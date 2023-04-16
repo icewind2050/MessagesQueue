6ce629a22aaf771c952370d69178b737d7c6700b
zhouhongwei <hongweizhou@foxmail.com>
2023-04-16 14:21:55 +0800

    DrawerTemp2

--- MessagesConsumer/src/main/java/zhouhongwei/Draw.java
+++ MessagesConsumer/src/main/java/zhouhongwei/Draw.java
@@ -1,6 +1,18 @@
 package zhouhongwei;
 
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.MessageConsumer;
+import javax.jms.Session;
+
 public class Draw implements Runnable{
+    private ConnectionFactory connectionFactory;
+    private Connection connection;
+    private Session session;
+    private MessageConsumer messageConsumer;
+
+
+
     @Override
     public void run() {
 
